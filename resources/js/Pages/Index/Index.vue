e78211f45dfa1000e275a86e7b584d17a53595db
eunbeann <ds5bean23@gmail.com>
2025-04-07 00:41:33 -0700

    add: page routing prep

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Index Page</h1>
+        <Link href="/hello">Show Page</Link>
+        <div>The message is {{ message }}</div>
+    </div>
+</template>
+
+<!-- logic section -->
+<script setup>
+import { Link } from '@inertiajs/vue3'
+defineProps([{ message: String }])
+</script>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Show page</h1>
+    <Link href="/">Index Page</Link>
+</template>
+
+<!-- logic section -->
+<script setup>
+import { Link } from '@inertiajs/vue3'
+</script>
